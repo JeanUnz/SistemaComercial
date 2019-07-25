5bc7ad3648d9b224aa07f9fe4a2c6be161b804c4
JeanUnz <jean.unzueta@unas.edu.pe>
2019-07-25 04:26:54 -0500

    areglando2..

--- resources/views/ventas/venta/create.blade.php
+++ resources/views/ventas/venta/create.blade.php
@@ -52,7 +52,7 @@
 				</div>
 			</div>
 			
-            <div class="row";
+            <div class="row">
                 <div class="panel panel-primary">
                     <div class="panel-body">
                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
